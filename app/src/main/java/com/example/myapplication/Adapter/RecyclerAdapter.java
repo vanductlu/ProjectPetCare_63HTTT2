a293dbb25c1a7001354f79236bd4cb4046a47201
k k <you@example.com>
2024-06-17 18:55:10 +0700

    Add Store and Cart activity adapter layout model

---
+++ app/src/main/java/com/example/myapplication/Adapter/RecyclerAdapter.java
@@ -0,0 +1,51 @@
+package com.example.myapplication.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.myapplication.R;
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private List<String> dataList;
+    private List<Integer> imageList;
+
+    public RecyclerAdapter(List<String> dataList, List<Integer> imageList) {
+        this.dataList = dataList;
+        this.imageList = imageList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.textView.setText(dataList.get(position));
+        holder.imageView.setImageResource(imageList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+        ImageView imageView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.item_text);
+            imageView = itemView.findViewById(R.id.item_image);
+        }
+    }
+}
