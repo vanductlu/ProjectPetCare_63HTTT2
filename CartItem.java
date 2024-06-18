de721370ec3775f984ca854a849c3cfc9261c5d4
KaTm <93910089+phamquockhanh1@users.noreply.github.com>
2024-06-18 15:59:11 +0700

    upup  "Cart ACt,adapter,model and layout"
    up "Cart ACt,adapter,model and layout"

---
+++ CartItem.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import com.example.myapplication.data.model.Product;
+
+import java.io.Serializable;
+
+public class CartItem implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Product product;
+    private int quantity;
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getTotalPrice() {
+        return product.getPrice() * quantity;
+    }
+}//11
---
+++ ProductAdapter.java
@@ -0,0 +1,80 @@
+package com.example.myapplication.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.data.model.Cart;
+import com.example.myapplication.R;
+import com.example.myapplication.data.model.Product;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+
+    private List<Product> productList;
+    private Cart cart;
+    private Context context;
+
+    public ProductAdapter(List<Product> productList, Cart cart, Context context) {
+        this.productList = productList;
+        this.cart = cart;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_product, parent, false);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        Product product = productList.get(position);
+        holder.productName.setText(product.getName());
+        holder.productDescription.setText(product.getDescription());
+        holder.productPrice.setText(product.getPrice() + " VND");
+        holder.productImage.setImageResource(product.getImageResId());
+
+        holder.addToCartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cart.addProduct(product);
+                Toast.makeText(context, product.getName() + " added to cart", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+    //change 1  sss
+
+    public static class ProductViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView productImage;
+        TextView productName;
+        TextView productPrice;
+        TextView productDescription;
+        Button addToCartButton;
+
+        public ProductViewHolder(@NonNull View itemView) {
+            super(itemView);
+            productImage = itemView.findViewById(R.id.product_image);
+            productName = itemView.findViewById(R.id.product_name);
+            productPrice = itemView.findViewById(R.id.product_price);
+            productDescription = itemView.findViewById(R.id.product_description);
+            addToCartButton = itemView.findViewById(R.id.add_to_cart_button);
+        }
+    }
+}
